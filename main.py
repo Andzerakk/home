a12ab3038846821bc89368847040850f9800d282
Andrew <andzerak@gmail.com>
2021-08-17 22:42:13 +0300

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+#name = input("Hello, enter your name")
+#print('Hello' + name)
+"""number = input("input first number")
+number2 = input("input second number")
+
+if number.isdigit() and number2.isdigit():
+    number = int(number)
+    number2 = int(number2)
+    n3 = number + number2
+    print(n3)
+else:
+    print("Enter only numbers")
+"""
+""""age = input("enter ur age")
+name = input("enter ur name")
+
+if age.isdigit():
+  d11 = int(age)
+  d1 = 100 - d11
+  print(d1)
+  d2 = 2021 + d1
+  print(name, "You will be 100 in", d2)
+else:
+  pass
+"""
+"""number = input("Input ur number")
+if number.isdigit():
+    number = int(number)
+    if number >= 0:
+        if (number % 2) == 0:
+            print("number is even")
+        else:
+            print("number is not even")
+    else:
+        pass
+else:
+    print("input only integer positive numbers")
+"""
+
